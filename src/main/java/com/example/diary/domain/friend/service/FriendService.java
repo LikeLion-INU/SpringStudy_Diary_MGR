4e44717c5e4ed12f49ae98e141c6ce93b646d1f6
minky <minkyeong02@daum.net>
2024-05-27 01:27:08 +0900

    [fix] ResponseDTO 처리

--- src/main/java/com/example/diary/domain/friend/service/FriendService.java
+++ src/main/java/com/example/diary/domain/friend/service/FriendService.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -27,23 +29,36 @@ public class FriendService {
         Optional<Users> user = userRepository.findById(id);
 
         if (user.isPresent()) {
-            String follower = user.get().getUserNickname();
+            //현재 entity에 팔로워 와 받는 사람이 존재한다면, 친구 요청 취소(즉 삭제)
+            Optional<Friend> friend = friendRepository.findByFollowerAndReceiver(user.get().getUserNickname(), receiver);
+            if (friend.isPresent()) friendRepository.deleteById(friend.get().getId());
+            else {
+                //존재 하지 않으면 새로운 친구 요청 제작
+                String follower = user.get().getUserNickname();
 
-            Friend friend = new Friend(follower, receiver, "N");
-            friendRepository.save(friend);
+                Friend f = new Friend(follower, receiver, "N");
+                friendRepository.save(f);
 
-            return FriendResponseDTO.toFriendDTO(friend);
-        } else return null;
+                return FriendResponseDTO.toFriendDTO(f);
+            }
+        }
+        return null;
     }
 
-    // 요청 건 친구 조회
-    public List<Friend> searchRequestFriend(String nickname) {
-        return friendRepository.findByFollower(nickname);
+    // 내가 요청 건 친구 조회
+    public List<FriendResponseDTO> searchRequestFriend(String nickname) {
+        List<Friend> friends = friendRepository.findByFollower(nickname);
+        return friends.stream()
+                .map(FriendResponseDTO::toFriendDTO)
+                .collect(Collectors.toList());
     }
 
     // 요청 받은 친구 조회
-    public List<Friend> searchReceiveFriend(String nickname) {
-        return friendRepository.findByReceiver(nickname);
+    public List<FriendResponseDTO> searchReceiveFriend(String nickname) {
+        List<Friend> friends = friendRepository.findByReceiver(nickname);
+        return friends.stream()
+                .map(FriendResponseDTO::toFriendDTO)
+                .collect(Collectors.toList());
     }
 
     // 친구 요청 승인
@@ -56,23 +71,35 @@ public class FriendService {
             Optional<Friend> res = friendRepository.findByFollowerAndReceiver(follower, receiver);
 
             if (res.isPresent()) {
-                String accept = "Y";
+                String accept = "";
+                //친구 요청이 존재하는데 N 이면 Y로
+                if (Objects.equals(res.get().getAccept(), "N")){
+                    accept = "Y";
+                } else {
+                    //Y 이면 N 으로
+                    accept = "N";
+                }
                 Friend friend = res.get();
                 friend.acceptFriend(follower, receiver, accept);
 
                 return FriendResponseDTO.toFriendDTO(friend);
-            } else return null;
-        } else return null;
+
+            }
+        }
+        return null;
     }
 
     // 친구 조회
-    public List<Friend> searchFriend(Long id) {
+    public List<FriendResponseDTO> searchFriend(Long id) {
         Optional<Users> users = userRepository.findById(id);
 
         if (users.isPresent()) {
             String follower = users.get().getUserNickname();
             // 현재 사용자의 닉네임으로 친구 조회 (ACCEPT 값이 Y인 것만)
-            return friendRepository.findByFollowerAndAccept(follower, "Y");
+            List<Friend> friends = friendRepository.findByFollowerAndAccept(follower, "Y");
+            return friends.stream()
+                    .map(FriendResponseDTO::toFriendDTO)
+                    .collect(Collectors.toList());
         } else return null;
     }
 
